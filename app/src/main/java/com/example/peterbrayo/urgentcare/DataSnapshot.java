a52bb849de0d033b521367053db253988b1be800
AZAMUKE DENISH <29928031+azamukedenish@users.noreply.github.com>
2018-07-02 13:11:21 +0300

    You need to follow up my comments in the subscriber.java file and therefore access firebase database services for the remaining portion of the code to work which should be pretty easy.

---
+++ app/src/main/java/com/example/peterbrayo/urgentcare/DataSnapshot.java
@@ -0,0 +1,7 @@
+package com.example.peterbrayo.urgentcare;
+
+class DataSnapshot {
+    public String getValue(Class<String> stringClass) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/peterbrayo/urgentcare/DatabaseError.java
@@ -0,0 +1,4 @@
+package com.example.peterbrayo.urgentcare;
+
+class DatabaseError {
+}
